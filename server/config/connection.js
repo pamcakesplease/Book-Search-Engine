140fa33c1334d9b0ee3ef706c2c6ee6b337599e8
pamcakesplease <pamelapimentel8@gmail.com>
2024-04-05 14:32:00 -0400

    render debugging

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks' , {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGODB_URI)
 
 module.exports = mongoose.connection;
