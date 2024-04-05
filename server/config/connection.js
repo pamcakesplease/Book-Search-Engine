d467f7776e249029e5e6405a3b3aa3c218f7a797
pamcakesplease <pamelapimentel8@gmail.com>
2024-04-05 15:50:17 -0400

    debugging connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,5 @@
 const mongoose = require('mongoose');
 
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
 
 module.exports = mongoose.connection;
