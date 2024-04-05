9626d55fe26fb5cbf373f914bc175edaf2bb1a52
pamcakesplease <pamelapimentel8@gmail.com>
2024-04-05 13:49:28 -0400

    debugging

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,3 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/booksearchengine');
 module.exports = mongoose.connection;
